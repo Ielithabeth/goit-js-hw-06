95c3456a698b438dba12013096fc1aa3581ff5c2
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-10-21 15:51:38 +0300

    add needed files

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const categories = document.querySelectorAll("li.item");
+
+console.log("Numbers of categories:", categories.length);
+
+const categoriesCaptions = document.querySelectorAll("h2");
+const  = document.querySelectorAll("ul");
+
+console.log("Category:", categoriesCaptions[0].textContent);
+console.log();
+
+console.log("Category:", categoriesCaptions[1].textContent);
+
+
+console.log("Category:", categoriesCaptions[2].textContent);
+
