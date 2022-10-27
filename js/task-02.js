d4710d8c3cc5a891ba674e32d0056a3dc7d7b34b
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-10-27 19:16:42 +0300

    2 task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul#ingredients");
+
+const addIngedients = ingredients.map(ingredient => {
+  const ingredientsRef = document.createElement("li");
+  ingredientsRef.textContent = ingredient;
+  ingredientsRef.classList.add("item");
+
+  return ingredientsRef;
+})
+
+list.append(...addIngedients);
+
