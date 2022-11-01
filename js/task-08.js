957110fe766b702deb86242df062a17b124d86ba
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-11-01 17:59:04 +0200

    8 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const {elements: { email, password }} = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+       alert("You mast fiil all fields!");
+    } else {
+        console.log({email: email.value, password: password.value});
+    }
+
+    loginForm.reset();
+});
 
