813d90ec5a460410367de1db1ad3406d71b83693
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-11-01 18:22:35 +0200

    9 task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
+function getRandomHexColor () {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const changeClBtn = document.querySelector("button");
+const colorName = document.querySelector("span");
+
+changeClBtn.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+
+  colorName.textContent = body.style.backgroundColor;
+})
