1fbb57124b4e25b5e64086f51ffb803a999e0f0a
Ielithabeth <lisa.pltrtskaya@gmail.com>
2022-11-04 15:44:19 +0200

    6 task correction

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const input = document.querySelector("#validation-input");
 
 input.addEventListener("blur", (event) => { 
     input.classList.add("invalid");
-    if (event.currentTarget.value.length >= input.dataset.length) {
+    if (event.currentTarget.value.length == input.dataset.length) {
         input.classList.replace("invalid", "valid");
         return;
     }
